31bf0bfaffa824070f20a34eafd6e37ce744c1b9
rexan1 <aminjirari@hotmail.com>
2019-01-14 11:54:00 +0100

    commit

---
+++ skript-2019-01-11-31.ps1
@@ -0,0 +1,7 @@
+﻿#this prints ou the phrase hello world in yellow text and blue background
+write-host -foregroundcolor yellow -BackgroundColor blue "Hello World"
+
+
+#This command opens a mircosoft help page about the function "clear host" and displays it using the web browser of your choice
+help  Clear-Host -Online
+
