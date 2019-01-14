febe68cbe456b2e2be13e3c49b95e8d4dd1b5453
rexan1 <aminjirari@hotmail.com>
2019-01-14 13:52:43 +0100

    commit

---
+++ skript-2019-01-11-59.ps1
@@ -0,0 +1,24 @@
+﻿$a=3
+Write-Host $a
+$a.GetType()
+
+[double] $b=3.3
+Write-Host $b
+$b.GetType()
+
+$c="3.3"
+Write-Host $c
+$c.GetType()
+
+[double] $d=$a + $b
+Write-Host $d
+$d.GetType()
+
+$a=$null
+$b=$null
+$d=$null
+[int] $a=3.4
+[int] $b=3.6
+$d=$a + $b
+Write-Host $d
+
