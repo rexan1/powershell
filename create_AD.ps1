6bee0c702c8ec25dddcf4c2ebd0e9671a021d342
rexan1 <aminjirari@hotmail.com>
2019-01-25 15:19:19 +0100

    commit

---
+++ create_AD.ps1
@@ -0,0 +1,84 @@
+﻿configuration NewDomain             
+{             
+   param             
+    (             
+        [Parameter(Mandatory)]             
+        [pscredential]$safemodeAdministratorCred,             
+        [Parameter(Mandatory)]            
+        [pscredential]$domainCred            
+    )             
+            
+    Import-DscResource -ModuleName xActiveDirectory             
+            
+    Node $AllNodes.Where{$_.Role -eq "Primary DC"}.Nodename             
+    {             
+            
+        LocalConfigurationManager            
+        {            
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true            
+        }            
+            
+        File ADFiles            
+        {            
+            DestinationPath = 'N:\NTDS'            
+            Type = 'Directory'            
+            Ensure = 'Present'            
+        }            
+                    
+        WindowsFeature ADDSInstall             
+        {             
+            Ensure = "Present"             
+            Name = "AD-Domain-Services"             
+        }            
+            
+        # Optional GUI tools            
+        WindowsFeature ADDSTools            
+        {             
+            Ensure = "Present"             
+            Name = "RSAT-ADDS"             
+        }            
+            
+        # No slash at end of folder paths            
+        xADDomain FirstDS             
+        {             
+            DomainName = $Node.DomainName             
+            DomainAdministratorCredential = $domainCred             
+            SafemodeAdministratorPassword = $safemodeAdministratorCred            
+            DatabasePath = 'N:\NTDS'            
+            LogPath = 'N:\NTDS'            
+            DependsOn = "[WindowsFeature]ADDSInstall","[File]ADFiles"            
+        }            
+            
+    }             
+}            
+            
+# Configuration Data for AD              
+$ConfigData = @{             
+    AllNodes = @(             
+        @{             
+            Nodename = "localhost"             
+            Role = "Primary DC"             
+            DomainName = "amin.lab"                               
+            RetryCount = 20              
+            RetryIntervalSec = 30            
+            PsDscAllowPlainTextPassword = $true            
+        }            
+    )             
+}             
+            
+NewDomain -ConfigurationData $ConfigData `
+    -safemodeAdministratorCred (Get-Credential -UserName '(Password Only)' `
+        -Message "New Domain Safe Mode Administrator Password") `
+    -domainCred (Get-Credential -UserName alpineskihouse\administrator `
+        -Message "New Domain Admin Credential")            
+            
+# Make sure that LCM is set to continue configuration after reboot            
+Set-DSCLocalConfigurationManager -Path .\NewDomain –Verbose            
+            
+# Build the domain            
+Start-DscConfiguration -Wait -Force -Path .\NewDomain -Verbose 
+
+
+
