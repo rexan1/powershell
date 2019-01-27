532116605830add2cd7d685ea33dd7e8be9520fb
rexan1 <46243104+rexan1@users.noreply.github.com>
2019-01-27 22:00:25 +0100

    Rename skript-2019-01-11-114.ps1 to 2019-01-11/skript-2019-01-11-104.ps1skript-2019-01-11-112.ps1skript-2019-01-11-114.ps1

--- 2019-01-11/skript-2019-01-11-104.ps1skript-2019-01-11-112.ps1skript-2019-01-11-114.ps1
+++ 2019-01-11/skript-2019-01-11-104.ps1skript-2019-01-11-112.ps1skript-2019-01-11-114.ps1
@@ -6,7 +6,15 @@ $elements
 #
 
 $elements.Protons
-#$elements.Protons | Measure -Average$file = Get-Content "C:\temp\elements2.csv"for ($count = 0; $count -lt $file.Length; $count++)
+
+#
+$elements.Protons | Measure -Average
+
+
+$file = Get-Content "C:\temp\elements2.csv"
+
+
+for ($count = 0; $count -lt $file.Length; $count++)
 
 {
     $col0 = ([string]$file[$count]).Split(',')[0]
@@ -16,4 +24,14 @@ $elements.Protons
     if ($col2 -eq $col3)
         {echo $col0} 
 
-    }#if ($elements.Protons = $elements.Neutrons){#Write-Host $elements #}
+    }
+
+
+
+
+
+
+
+#if ($elements.Protons = $elements.Neutrons){
+#Write-Host $elements 
+#}
